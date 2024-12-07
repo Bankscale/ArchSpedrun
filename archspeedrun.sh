c8732efadf05c11b5ea66d92260bab18ffb305e3
Bankscale <squirtsquirted@gmail.com>
2024-12-07 01:14:33 -0600

    Added a check before formatting disks

--- archspeedrun.sh
+++ archspeedrun.sh
@@ -5,63 +5,73 @@ if [ "$EUID" != 0 ]
     exit
 fi
 
-# Removes all partitions on drive
-sgdisk --zap-all "$1"
+  # Added check before wiping disks
+while true; do
+    read -p "Is $1 the correct disk? " yn
+    case $yn in
+      [Yy]* ) install $1 $2 $3; break;;
+      [Nn]* ) exit;;
+    esac
+done
 
-# Creates boot, root, swap, home partitions
-sgdisk -n 1:0:+1G -t 1:ef00 "$1"
-sgdisk -n 2:0:+100G -t 1:8300 "$1"
-sgdisk -n 3:-4G:0 -t 1:8200 "$1"
-sgdisk -n 4:0:0 -t 1:8300 "$1"
+install() {
+  # Removes all partitions on drive
+  sgdisk --zap-all "$1"
 
-# Sorts the partitions
-sgdisk --sort "$1"
+  # Creates boot, root, swap, home partitions
+  sgdisk -n 1:0:+1G -t 1:ef00 "$1"
+  sgdisk -n 2:0:+100G -t 1:8300 "$1"
+  sgdisk -n 3:-4G:0 -t 1:8200 "$1"
+  sgdisk -n 4:0:0 -t 1:8300 "$1"
 
-# Checks for drive type
-if echo "$1" | grep -q "nmve"
-then
+  # Sorts the partitions
+  sgdisk --sort "$1"
 
-    # Formats the partitions
-    mkfs.fat -F 32 "$1"p1
-    mkfs.ext4 "$1"p2
-    mkfs.ext4 "$1"p3
-    mkswap "$1"p4
-    swapon "$1"p4
-    
-    # Mounts the drives
-    mount "$1"p2 /mnt
-    mkdir /mnt/boot
-    mount "$1"p1 /mnt/boot
-    mkdir /mnt/home
-    mount "$1"p3 /mnt/home
- else
+  # Checks for drive type
+  if echo "$1" | grep -q "nmve"
+  then
 
-    # Formats the partitions
-    mkfs.fat -F 32 "$1"1
-    mkfs.ext4 "$1"2
-    mkfs.ext4 "$1"3
-    mkswap "$1"4
-    swapon "$1"4
+      # Formats the partitions
+      mkfs.fat -F 32 "$1"p1
+      mkfs.ext4 "$1"p2
+      mkfs.ext4 "$1"p3
+      mkswap "$1"p4
+      swapon "$1"p4
+      
+      # Mounts the drives
+      mount "$1"p2 /mnt
+      mkdir /mnt/boot
+      mount "$1"p1 /mnt/boot
+      mkdir /mnt/home
+      mount "$1"p3 /mnt/home
+   else
 
-    # Mounts the drives
-    mount "$1"2 /mnt
-    mkdir /mnt/boot
-    mount "$1"1 /mnt/boot
-    mkdir /mnt/home
-    mount "$1"3 /mnt/home
-fi
+      # Formats the partitions
+      mkfs.fat -F 32 "$1"1
+      mkfs.ext4 "$1"2
+      mkfs.ext4 "$1"3
+      mkswap "$1"4
+      swapon "$1"4
 
-# Installs all "base" packages
-pacstrap -G /mnt base base-devel linux linux-firmware linux-headers vim zsh kitty sudo grub efibootmgr rofi dhcpcd networkmanager hyprland git wget curl openssh gdisk tldr btop fzf zsh-syntax-highlighting pipewire pipewire-alsa pipewire-jack pipewire-pulse wireplumber python python-virtualenv sddm os-prober
+      # Mounts the drives
+      mount "$1"2 /mnt
+      mkdir /mnt/boot
+      mount "$1"1 /mnt/boot
+      mkdir /mnt/home
+      mount "$1"3 /mnt/home
+  fi
+
+  # Installs all "base" packages
+  pacstrap -G /mnt base base-devel linux linux-firmware linux-headers vim zsh kitty sudo grub efibootmgr rofi dhcpcd networkmanager hyprland git wget curl openssh gdisk tldr btop fzf zsh-syntax-highlighting pipewire pipewire-alsa pipewire-jack pipewire-pulse wireplumber python python-virtualenv sddm os-prober
 
 
-genfstab -U /mnt > /mnt/etc/fstab
+  genfstab -U /mnt > /mnt/etc/fstab
 
-# Copies post-chroot part two onto /mnt
-cp ./archspeedrunp2.sh /mnt
+  # Copies post-chroot part two onto /mnt
+  cp ./archspeedrunp2.sh /mnt
 
-# Executes second part after chroot
-arch-chroot /mnt ./archspeedrunp2.sh "$2" "$3"
+  # Executes second part after chroot
+  arch-chroot /mnt ./archspeedrunp2.sh "$2" "$3"
 
-rm "$0"
-
+  rm "$0"
+}
